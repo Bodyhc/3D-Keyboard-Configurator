77c9e9feaacf954aa7725653ee2db4679bd40884
Abdullah M Soliman <94320243+Bodyhc@users.noreply.github.com>
2025-03-05 03:20:13 +0200

    keyboard-3d.tsx

--- components/keyboard-3d.tsx
+++ components/keyboard-3d.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState } from 'react';
 import { Group } from "three";
 import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader, MeshStandardMaterial, Color } from 'three';
+import { useTexture } from "@react-three/drei";
+
 import { KeyboardConfig } from '@/lib/types';
 import { Box, Text } from '@react-three/drei';
 
