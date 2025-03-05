c9f75bd383d3af50ccfaa66c6adbd0c6b26443b6
Abdullah M Soliman <94320243+Bodyhc@users.noreply.github.com>
2025-03-05 03:59:50 +0200

    types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,4 @@
+import { LAYOUTS, SWITCH_COLORS, KEYCAP_MATERIALS, KEYBOARD_COLORS } from '@/components/keyboard-3d';
 export interface KeyboardConfig {
   layout: keyof typeof LAYOUTS;
   switches: keyof typeof SWITCH_COLORS;
