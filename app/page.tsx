e9b5354fc617c275d5b8c1c7cc9bd8832e08433f
Abdullah M Soliman <94320243+Bodyhc@users.noreply.github.com>
2025-03-05 05:26:21 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import KeyboardCustomizer from '@/components/keyboard-customizer';
+import KeyboardCustomizer from "@/components/keyboard-customizer";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
-      <KeyboardCustomizer />
+      <>
+        <Toaster position="top-right" reverseOrder={false} />
+        <KeyboardCustomizer />
+      </>
     </main>
   );
 }
